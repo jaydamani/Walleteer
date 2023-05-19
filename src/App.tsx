9c40299e663aff2234de583582aa3f04ffaf7f8e
jaydamani <jaydamani567@gmail.com>
2023-05-20 00:06:16 +0530

    feat: able to set theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import "react-native-gesture-handler";
-import { DarkTheme } from "@react-navigation/native";
-import RootNavigator from "./navigation/RootNavigator";
 import { registerRootComponent } from "expo";
+import { useState } from "react";
+import "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import RootNavigator from "./navigation/RootNavigator";
 
 function App() {
+  const [isDark, setTheme] = useState(true);
   return (
     <SafeAreaProvider>
-      <RootNavigator theme={DarkTheme} />
+      <RootNavigator setTheme={setTheme} isDark={isDark} />
     </SafeAreaProvider>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,26 @@
-import { Text } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { useTheme } from "@react-navigation/native";
+import { Button, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export function Home({}) {
+export function Home({ isDark, setTheme }) {
+  const Tab = createMaterialTopTabNavigator();
+  const theme = useTheme();
   return (
     <SafeAreaView>
-      <Text style={{color: 'red', justifyContent: 'center'}}>Home</Text>
+      <Tab.Navigator>
+        <Tab.Screen name="Test1">
+          {() => (
+            <Button title={"Test" + isDark + setTheme} onPress={() => setTheme(!isDark)}></Button>
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Test2">
+          {() => <Text style={{ color: theme.colors.text }}>test</Text>}
+        </Tab.Screen>
+        <Tab.Screen name="Test3">
+          {() => <Text style={{ color: theme.colors.text }}>test</Text>}
+        </Tab.Screen>
+      </Tab.Navigator>
     </SafeAreaView>
   );
 }
