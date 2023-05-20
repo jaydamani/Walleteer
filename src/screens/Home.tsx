42916459886f6bd8df5af3022195ff8a536989c0
jaydamani <jaydamani567@gmail.com>
2023-05-20 13:14:52 +0530

    fix: tabs in android

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,6 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 import { DarkTheme, DefaultTheme } from "@react-navigation/native";
 import { useContext } from "react";
 import { Button } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemeContext } from "@Context/settings";
 import { MainText } from "@Components";
 import { DrawerNavigationOptions } from "@react-navigation/drawer";
@@ -13,26 +12,34 @@ export const options: DrawerNavigationOptions = {
   drawerIcon: ({ color }) => <Icon name="home" size={24} color={color} />,
 };
 
-export function Component({ navigation, route }) {
-  const Tab = createMaterialTopTabNavigator();
+function Butt({}) {
   const { setTheme, theme } = useContext(ThemeContext);
   return (
-    <SafeAreaView>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Test1"
-          component={() => (
-            <Button
-              title={"Test"}
-              onPress={() =>
-                setTheme(theme == DefaultTheme ? DarkTheme : DefaultTheme)
-              }
-            ></Button>
-          )}
-        />
-        <Tab.Screen name="Test2" component={() => <MainText>test</MainText>} />
-        <Tab.Screen name="Test3" component={() => <MainText>test</MainText>} />
-      </Tab.Navigator>
-    </SafeAreaView>
+    <Button
+      title={"Test"}
+      onPress={() => setTheme(theme == DefaultTheme ? DarkTheme : DefaultTheme)}
+    ></Button>
+  );
+}
+
+function Tex({}) {
+  return <MainText>test</MainText>;
+}
+
+export function Component({ navigation, route }) {
+  const Tab = createMaterialTopTabNavigator();
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarContentContainerStyle: {
+          borderTopWidth: 0,
+          shadowColor: "transparent",
+        },
+      }}
+    >
+      <Tab.Screen name="Test1" component={Butt} />
+      <Tab.Screen name="Test2" component={Tex} />
+      <Tab.Screen name="Test3" component={Tex} />
+    </Tab.Navigator>
   );
 }
