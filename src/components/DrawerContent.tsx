e869fb637257874cc3eb4749d0acf3024d8c9286
jaydamani <jaydamani567@gmail.com>
2023-05-20 10:04:17 +0530

    refactor: move drawercontent to separate component

---
+++ src/components/DrawerContent.tsx
@@ -0,0 +1,23 @@
+import {
+  DrawerContentComponentProps,
+  DrawerItemList
+} from "@react-navigation/drawer";
+import {
+  SafeAreaView,
+  useSafeAreaInsets
+} from "react-native-safe-area-context";
+
+export function DrawerContent({
+  state, navigation, descriptors,
+}: DrawerContentComponentProps) {
+  const insets = useSafeAreaInsets();
+  return (
+    <SafeAreaView style={[{ paddingTop: insets.top + 10 }]}>
+      <SafeAreaView style={{ height: 150 }}></SafeAreaView>
+      <DrawerItemList
+        state={state}
+        navigation={navigation}
+        descriptors={descriptors} />
+    </SafeAreaView>
+  );
+}
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,38 +1,14 @@
 import {
-  DrawerContentComponentProps,
-  DrawerItemList,
   createDrawerNavigator,
 } from "@react-navigation/drawer";
 import {
   DarkTheme,
-  DefaultTheme,
   NavigationContainer,
 } from "@react-navigation/native";
-import {
-  SafeAreaView,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
 import { screens } from "@Screens";
 import { useState } from "react";
 import { ThemeContext } from "@Context/settings";
-
-function DrawerContent({
-  state,
-  navigation,
-  descriptors,
-}: DrawerContentComponentProps) {
-  const insets = useSafeAreaInsets();
-  return (
-    <SafeAreaView style={[{ paddingTop: insets.top + 10 }]}>
-      <SafeAreaView style={{ height: 150 }}></SafeAreaView>
-      <DrawerItemList
-        state={state}
-        navigation={navigation}
-        descriptors={descriptors}
-      />
-    </SafeAreaView>
-  );
-}
+import { DrawerContent } from "@Components";
 
 export default function RootNavigator() {
   const Drawer = createDrawerNavigator();
