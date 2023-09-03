1bf528bb9c651fd8696222d2ec050bb107cc2e2f
jaydamani <jaydamani567@gmail.com>
2023-09-03 19:04:33 +0530

    refactor

--- src/screens/TransactionForm.tsx
+++ src/screens/TransactionForm.tsx
@@ -3,33 +3,45 @@ import { transactions } from '@database';
 import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useEffect, useState } from 'react';
-import { Button } from 'react-native-ui-lib';
-import NotLoaded from './NotLoaded';
+import { Button } from 'react-native-paper';
+import LoadingScreen from './LoadingScreen';
 
 type Props = NativeStackScreenProps<Screens, 'transactionForm'>;
 export function getTransactionFormOptions({
-  navigation,
   route,
 }: Props): NativeStackNavigationOptions {
-  function headerRight() {
-    return <Button onPress={() => navigation.goBack()} label="Save" />;
-  }
   return {
-    headerRight,
+    headerRight: () => HeaderRight({}),
     headerTitle: route.params.transaction
       ? 'Edit Transaction'
       : 'New Transaction',
   };
 }
 
-export function TransactionForm({ route }: Props) {
+function HeaderRight({ onPress }: { onPress?: () => void }) {
+  return (
+    <Button onPress={onPress} mode="elevated">
+      Save
+    </Button>
+  );
+}
+
+export function TransactionForm({ route, navigation }: Props) {
   const transactionId = route.params.transaction;
   let [transaction, setTransaction] = useState<any>(null);
+  function onPress() {}
+  useEffect(
+    () =>
+      navigation.setOptions({
+        headerRight: () => HeaderRight({ onPress }),
+      }),
+    [navigation],
+  );
   useEffect(() => {
     if (!transactionId) return setTransaction({});
 
     transactions.find(transactionId).then(setTransaction);
   }, [transactionId]);
 
-  return transaction ? <></> : <NotLoaded />;
+  return transaction ? <></> : <LoadingScreen />;
 }
