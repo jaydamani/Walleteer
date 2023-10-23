0aede3fc5669bb93288f468ba19321ff57704857
jaydamani <jaydamani567@gmail.com>
2023-10-24 04:05:12 +0530

    feat: wait 3 seconds before showing loading screen

--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
 
 export default function LoadingScreen() {
-  return (
+  const [show, setShow] = useState(false);
+  useEffect(function () {
+    setTimeout(() => setShow(true), 3000);
+  }, []);
+  return show ? (
     <View style={[styles.container, StyleSheet.absoluteFill]}>
       <ActivityIndicator animating size="large" />
     </View>
+  ) : (
+    <></>
   );
 }
 
