4d628569cca1466047118e5b0e2fc56b8c6e489a
jaydamani <jaydamani567@gmail.com>
2023-11-21 14:55:45 +0530

    deps: add new deps

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import { AppRegistry } from 'react-native';
 import { App } from './src';
 import { name as appName } from './app.json';
+import { en, registerTranslation } from 'react-native-paper-dates';
 
+registerTranslation('en', en);
 AppRegistry.registerComponent(appName, () => App);
