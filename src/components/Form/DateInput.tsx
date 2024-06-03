087699b7b12b3c101e3a4ea53bc5674f2be24c6d
jaydamani <jaydamani567@gmail.com>
2024-06-04 00:32:24 +0530

    feat: replace react-native-paper-dates with @react-native-community/datetimepicker

--- src/components/Form/DateInput.tsx
+++ src/components/Form/DateInput.tsx
@@ -1,4 +1,4 @@
-import { HelperText } from 'react-native-paper';
+import { HelperText, TextInput } from 'react-native-paper';
 import {
   FieldValues,
   Path,
@@ -6,35 +6,59 @@ import {
   UseControllerProps,
   UseFormReturn,
 } from 'react-hook-form';
-import { DatePickerInput } from 'react-native-paper-dates';
-import { DatePickerInputProps } from '@lib/types';
+import { useState } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Keyboard } from 'react-native';
 
 export type DateInputProps<
   T extends FieldValues = FieldValues,
   K extends Path<T> = Path<T>,
 > = UseControllerProps<T, K> &
-  Partial<DatePickerInputProps> & {
+  Partial<any> & {
     setError: UseFormReturn<T, K>['setError'];
   };
 
 export function DateInput<
   T extends FieldValues = FieldValues,
   K extends Path<T> = Path<T>,
->({ setError, ...props }: DateInputProps<T, K>) {
-  const { field, fieldState } = useController(props);
+>({ ...props }: DateInputProps<T, K>) {
+  const {
+    field: { onChange, value, ...field },
+    fieldState,
+  } = useController(props);
+  const [open, setOpen] = useState(false);
+  if (open) Keyboard.dismiss();
   return (
     <>
-      <DatePickerInput
-        inputMode="start"
-        mode='outlined'
-        locale="en"
-        onValidationError={message =>
-          message && setError(props.name, { message })
+      <TextInput
+        right={
+          <TextInput.Icon
+            onPress={() => {
+              setOpen(true);
+            }}
+            icon="calendar"
+          />
         }
-        hideValidationErrors
-        {...field}
+        mode="outlined"
         {...props}
+        {...field}
+        value={value.toLocaleDateString('default', {
+          month: '2-digit',
+          year: '2-digit',
+          day: '2-digit',
+        })}
       />
+      {open && (
+        <DateTimePicker
+          value={value}
+          mode="date"
+          onChange={(_, date) => {
+            setOpen(false);
+            if (!date) return;
+            onChange(date);
+          }}
+        />
+      )}
       {fieldState.invalid && (
         <HelperText type="error">
           {fieldState.error?.message || 'This field is incorrect!'}
