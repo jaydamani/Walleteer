ff3c2a8dba230252802e773b53bd0cb250bbb234
jaydamani <jaydamani567@gmail.com>
2024-06-15 13:09:32 +0530

    chores: cleanup

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,25 +1,4 @@
 import { ColumnSchema } from '@nozbe/watermelondb';
-import { PossibleInputTypes } from 'react-native-paper-dates/lib/typescript/Time/timeUtils';
-
-export type { DatePickerInputProps } from 'react-native-paper-dates/lib/typescript/Date/DatePickerInput.shared';
-export type TimePickerProps = {
-  locale?: undefined | string;
-  label?: string;
-  uppercase?: boolean;
-  cancelLabel?: string;
-  confirmLabel?: string;
-  hours?: number | undefined;
-  minutes?: number | undefined;
-  visible: boolean | undefined;
-  onDismiss: () => any;
-  onConfirm: (hoursAndMinutes: { hours: number; minutes: number }) => any;
-  animationType?: 'slide' | 'fade' | 'none';
-  keyboardIcon?: string;
-  clockIcon?: string;
-  use24HourClock?: boolean;
-  inputFontSize?: number;
-  defaultInputType?: PossibleInputTypes;
-};
 
 export type ColumnList<T> = (ColumnSchema & {
   name: keyof T;
